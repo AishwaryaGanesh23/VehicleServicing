34137986837062d4fb10d3af38ff11b2e6d15e2f
melrick04 <mca.1924@unigoa.ac.in>
2021-02-14 21:46:10 +0530

    sc.v2

--- ServiceCenter/booking.php
+++ ServiceCenter/booking.php
@@ -14,96 +14,121 @@
 
 <body>
 
-    <form id="" method="post">
+    <div class="img">
+        <img src="img/banner.jpg">
+    </div>
 
-        <div class="form-section">
+    <br>
 
-            <input type="text" name="name" autocomplete="off" required />
-            <label for="name" class="label-name">
-                <span class="content-name">
-                    Name
-                </span>
-            </label>
-        </div>
+    <div class="container">
 
-        <div class="form-section">
-            <input type="tel" name="contact" id="contact" required />
-            <label for="contact" class="label-name">
-                <span class="content-name">Contact</span>
-            </label>
-        </div>
+        <div class="background">
 
-        <div class="form-section">
-            <input type="text" name="address" id="address" required />
-            <label for="address" class="label-name">
-                <span class="content-name">Address</span>
-            </label>
-        </div>
+            <form id="" method="post">
+
+                <div class="form-section">
+
+                    <input type="text" name="name" autocomplete="off" required />
+                    <label for="name" class="label-name">
+                        <span class="content-name">
+                            Name
+                        </span>
+                    </label>
+                </div>
+                <br>
+                <br>
+                <div class="form-section">
+                    <input type="tel" name="contact" id="contact" required />
+                    <label for="contact" class="label-name">
+                        <span class="content-name">Contact</span>
+                    </label>
+                </div>
+                <br>
+                <br>
+                <div class="form-section">
+                    <input type="text" name="address" id="address" required />
+                    <label for="address" class="label-name">
+                        <span class="content-name">Address</span>
+                    </label>
+                </div>
+                <br>
+                <br>
+
+                <div class="form-section">
+                    <input type="email" name="email" id="email" required />
+                    <label for="email" class="label-name">
+                        <span class="content-name">Email Address</span>
+                    </label>
+                </div>
+                <br>
+                <br>
+
+                <div class="form-section">
+                    <input type="text" name="model" id="model" required />
+                    <label for="model" class="label-name">
+                        <span class="content-name">Vehicle Model</span>
+                    </label>
+                </div>
+                <br>
+                <br>
+
+                <div class="form-section">
+                    <input type="text" name="reg" id="reg" required />
+                    <label for="reg" class="label-name">
+                        <span class="content-name">Vehicle Registration</span>
+                    </label>
+                </div>
+                <br>
+                <br>
+                <div class="form-section">
+                    <input type="number" name="distance" id="distance" required />
+                    <label for="distance" class="label-name">
+                        <span class="content-name">Total Kilometers</span>
+                    </label>
+                </div>
+                <br>
+                <br>
+
+                <div class="form-section">
+                    <input type="date" name="date" id="date" onfocus="(this.type='date')" onfocusout="(this.type='text')" required />
+                    <label for="date" class="label-name">
+                        <span class="content-name">Appointment Date</span>
+                    </label>
+                </div>
+                <br>
+                <br>
 
 
-        <div class="form-section">
-            <input type="email" name="email" id="email" required />
-            <label for="email" class="label-name">
-                <span class="content-name">Email Address</span>
-            </label>
-        </div>
+                <div class="form-group">
+                    <label>Services:</label>
+                    <br>
+                    <br>
+                    <label class="checkbox-inline"><input type="checkbox" value="">Option 1</label>
+                    <label class="checkbox-inline"><input type="checkbox" value="">Option 2</label>
+                    <label class="checkbox-inline"><input type="checkbox" value="">Option 3</label>
+                </div>
+                <br>
+                <br>
 
 
-        <div class="form-section">
-            <input type="text" name="model" id="model" required />
-            <label for="model" class="label-name">
-                <span class="content-name">Vehicle Model</span>
-            </label>
-        </div>
+                <div class="form-group">
+                    <label for="desc">Specific Request:</label>
+                    <br>
+                    <textarea rows="4" cols="40" name="address" id="address" required></textarea>
+                </div>
+                <br>
+                <br>
 
 
-        <div class="form-section">
-            <input type="text" name="reg" id="reg" required />
-            <label for="reg" class="label-name">
-                <span class="content-name">Vehicle Registration</span>
-            </label>
-        </div>
-
-        <div class="form-section">
-            <input type="number" name="distance" id="distance" required />
-            <label for="distance" class="label-name">
-                <span class="content-name">Total Kilometers</span>
-            </label>
-        </div>
-
-
-        <div class="form-section">
-            <input type="date" name="date" id="date" onfocus="(this.type='date')" onfocusout="(this.type='text')" required />
-            <label for="date" class="label-name">
-                <span class="content-name">Appointment Date</span>
-            </label>
-        </div>
-        <br>
-
-
-        <div class="form-group">
-            <label>Services:</label>
+                <div class="form-group">
+                    <button type="button" name="submit" id="submit">Place Appointment</button>
+                </div>
+            </form>
             <br>
             <br>
-            <label class="checkbox-inline"><input type="checkbox" value="">Option 1</label>
-            <label class="checkbox-inline"><input type="checkbox" value="">Option 2</label>
-            <label class="checkbox-inline"><input type="checkbox" value="">Option 3</label>
-        </div>
-        <br>
-
-
-        <div class="form-group">
-            <label for="desc">Specific Request:</label>
             <br>
-            <textarea rows="4" cols="40" name="address" id="address" required></textarea>
         </div>
-        <br>
-
-
-        <div class="form-group">
-            <button type="button" name="submit" id="submit">Place Appointment</button>
-        </div>
-    </form>
+    </div>
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
